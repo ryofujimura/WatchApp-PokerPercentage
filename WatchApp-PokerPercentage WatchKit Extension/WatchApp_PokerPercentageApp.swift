8f49d7bc430505e0c1be6771c2754d7734b9c267
Ryo Fuj <ryo176999@gmail.com>
2022-01-30 01:27:09 -0800

    Initial commit

---
+++ WatchApp-PokerPercentage WatchKit Extension/WatchApp_PokerPercentageApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchApp_PokerPercentageApp.swift
+//  WatchApp-PokerPercentage WatchKit Extension
+//
+//  Created by ryo fuj on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_PokerPercentageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ WatchApp-PokerPercentage/WatchApp_PokerPercentageApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp_PokerPercentageApp.swift
+//  WatchApp-PokerPercentage
+//
+//  Created by ryo fuj on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp_PokerPercentageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
